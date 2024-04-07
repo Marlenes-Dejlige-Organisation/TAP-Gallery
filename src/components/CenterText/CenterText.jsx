4b0ffb45f8b546bb9173dca2c94c13d22fdcdee7
MarlaRey <hejmarlenehej@gmail.com>
2024-04-07 21:44:47 +0200

    2 artists added

--- src/components/CenterText/CenterText.jsx
+++ src/components/CenterText/CenterText.jsx
@@ -1,6 +1,66 @@
 import styles from './CenterText.module.scss'
+import React, { useState, useEffect } from 'react';
+import * as contentful from 'contentful';
+
+const client = contentful.createClient({
+    space: 'd6annblvb5fr',
+    environment: 'master',
+    accessToken: 'Kd4rwoRDmJcJkY3uSJE2PTu29mH1F-ocq4JR1Vfo0dU'
+});
+
+export const CenterTextSection2 = () => {
+    const [bannerUrl, setBannerUrl] = useState('');
+    const [artistName, setArtistName] = useState("");
 
 
+    useEffect(() => {
+        client.getEntry('5AQvpE7kD17JrJAdfXWRgW')
+            .then((entry) => {
+                setArtistName(entry.fields.artist[0]);
+
+                setBannerUrl(entry.fields.banner[1].fields.file.url);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <section className={styles.sectionWrapperP} style={{ backgroundImage: `url(${bannerUrl})` }}>
+            <div className={styles.headerInfo}>
+            <h2>{artistName}</h2>
+                <p>us voluptate maiores est quis sectetur adipisicing elit. Labore in explicabo error praesentium a aperiam dicta ipsum sit itaque delectus id architecto, quod, quisquam iusto atque at? Fuga, enim sed.</p>
+                <button>Læs mere</button>
+            </div>
+        </section>
+    );
+}
+
+export const CenterTextSection3 = () => {
+    const [bannerUrl, setBannerUrl] = useState('');
+    const [artistName, setArtistName] = useState("");
+
+    useEffect(() => {
+        client.getEntry('5AQvpE7kD17JrJAdfXWRgW')
+            .then((entry) => {
+                setArtistName(entry.fields.artist[1]);
+                setBannerUrl(entry.fields.banner[2].fields.file.url);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <section className={styles.sectionWrapperP2} style={{ backgroundImage: `url(${bannerUrl})` }}>
+            <div className={styles.headerInfo}>
+                <h2>{artistName}</h2>
+                <p>us voluptate maiores est quis sectetur adipisicing elit. Labore in explicabo error praesentium a aperiam dicta ipsum sit itaque delectus id architecto, quod, quisquam iusto atque at? Fuga, enim sed.</p>
+                <button>LÆS MERE</button>
+            </div>
+        </section>
+    );
+}
 
 export const CenterTextHeader = () => {
     return <section className={styles.sectionWrapper} >
@@ -22,22 +82,3 @@ export const CenterTextSection = () => {
         </section>
 }
 
-export const CenterTextSection2 = () => {
-    return <section className={styles.sectionWrapperP} >
-        <div className={styles.headerInfo}>
-        <h2>ONE 2 ONE</h2>
-        <p>us voluptate maiores est quis sectetur adipisicing elit. Labore in explicabo error praesentium a aperiam dicta ipsum sit itaque delectus id architecto, quod, quisquam iusto atque at? Fuga, enim sed.</p>
-        <button>BOOK TID</button>
-        </div>
-        </section>
-}
-
-export const CenterTextSection3 = () => {
-    return <section className={styles.sectionWrapperP2} >
-        <div className={styles.headerInfo}>
-        <h2>ONE 2 ONE</h2>
-        <p>us voluptate maiores est quis sectetur adipisicing elit. Labore in explicabo error praesentium a aperiam dicta ipsum sit itaque delectus id architecto, quod, quisquam iusto atque at? Fuga, enim sed.</p>
-        <button>LÆS MERE</button>
-        </div>
-        </section>
-}
